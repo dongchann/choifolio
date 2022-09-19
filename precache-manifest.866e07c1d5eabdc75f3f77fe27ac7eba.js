33ac6aa66da2db203f24014a790d36ba09f047dc
dongchann <dongchann91@gmail.com>
2022-09-19 23:21:15 +0900

    Updates

---
+++ precache-manifest.866e07c1d5eabdc75f3f77fe27ac7eba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6389441488a5a9d5574",
+    "url": "/choifolio/static/css/main.09162fdd.chunk.css"
+  },
+  {
+    "revision": "e6389441488a5a9d5574",
+    "url": "/choifolio/static/js/main.f08350e4.chunk.js"
+  },
+  {
+    "revision": "b85e20740ece2dd5ba23",
+    "url": "/choifolio/static/js/runtime~main.def60e75.js"
+  },
+  {
+    "revision": "4672aad06bd9feb0faa0",
+    "url": "/choifolio/static/js/2.6f5761f9.chunk.js"
+  },
+  {
+    "revision": "57b409c86cfae2784249",
+    "url": "/choifolio/static/js/3.053b0c51.chunk.js"
+  },
+  {
+    "revision": "77228b3d696312d3096eea1b4419adda",
+    "url": "/choifolio/index.html"
+  }
+];
